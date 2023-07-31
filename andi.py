f96ba52a7107ec8a6acefa2194c0b53d4fef4527
stefkwan-h8 <schristine@hacktiv8.com>
2023-07-31 21:39:58 +0700

    sesi 3 ongoing

---
+++ andi.py
@@ -0,0 +1,16 @@
+nama = "Andhika Widjaja"
+
+umur = 27
+
+background = {
+    "edukasi": "S1",
+    "keluaga": "berkeluarga",
+    "mencari_pekerjaan": True,
+    "tahun_lahir": 1996
+}
+
+
+def kenalan():
+    print("nama saya", nama)
+    print("saya lahir di tahun", background['tahun_lahir'])
+    print("salam kenal")
